a7b9740777f6038e2bd934ec447bf4e423485bbc
wangwentao <123456Wwt>
2022-10-25 15:42:16 +0800

    node 监听端口需和nginx配置的端口相同

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -80,7 +80,7 @@ const server = http.createServer(function (request, response) {
     }
   });
 });
-// 让服务器监听8080端口:
-server.listen(8080);
+// 让服务器监听8089端口:
+server.listen(8089);
 
-console.log('Server is running at http://127.0.0.1:8080/');
+console.log('Server is running at http://127.0.0.1:8089/');
