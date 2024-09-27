3975dcf65ada088bd7dbf4c2a42fef6ab1b012c3
wangwentao <wangwentao>
2024-09-27 15:16:47 +0800

    测试提交1

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -17,7 +17,9 @@ const http = require('http');
 const path = require('path');
 const url = require('url');
 const {readDirSync} = require('./allmenu');
-let root = path.resolve('../');
+let root = path.resolve('./');
+console.log(root);
+console.log(11111);
 
 // let workDir = path.resolve('../'); // . 当前目录， ../上一级目录
 // console.log(workDir);
